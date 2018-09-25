be90142f08fd65f07c8ed08a7176f6323792576e
xunwanw <x.wang153@student.unimelb.edu.au>
2018-09-25 16:35:51 +1000

    initial myTeam

--- pacman-contest/myTeam.py
+++ pacman-contest/myTeam.py
@@ -22,7 +22,7 @@ import game
 #################
 
 def createTeam(firstIndex, secondIndex, isRed,
-               first = 'DummyAgent', second = 'DummyAgent'):
+               first = 'Attacker', second = 'Defender'):
   """
   This function should return a list of two agents that will form the
   team, initialized using firstIndex and secondIndex as their agent
@@ -44,44 +44,52 @@ def createTeam(firstIndex, secondIndex, isRed,
 ##########
 # Agents #
 ##########
+class Actions():
 
-class DummyAgent(CaptureAgent):
-  """
-  A Dummy agent to serve as an example of the necessary agent structure.
-  You should look at baselineTeam.py for more details about how to
-  create an agent as this is the bare minimum.
-  """
+  def getSuccessor(self, gameState, action):
 
-  def registerInitialState(self, gameState):
-    """
-    This method handles the initial setup of the
-    agent to populate useful fields (such as what team
-    we're on).
+    return
 
-    A distanceCalculator instance caches the maze distances
-    between each pair of positions, so your agents can use:
-    self.distancer.getDistance(p1, p2)
+  def evaluate(self):
+    return
 
-    IMPORTANT: This method may run for at most 15 seconds.
-    """
+  def getFeatures(self):
+    return
 
-    '''
-    Make sure you do not delete the following line. If you would like to
-    use Manhattan distances instead of maze distances in order to save
-    on initialization time, please take a look at
-    CaptureAgent.registerInitialState in captureAgents.py.
-    '''
-    CaptureAgent.registerInitialState(self, gameState)
+  def getWeights(self):
+    return
 
-    '''
-    Your initialization code goes here, if you need any.
-    '''
+class getOffensiveActions(Actions):
+  def __init__(self):
+    return
 
+  def getFeatures(self):
+    #compute the distance to nearest boundary
+    #compute score from successor state
+    #compute the nearest food
+    #compute the nearest capsule
+    #compute the closet ghost
+    return
+  def getWeights(self):
+    return
 
+  def simulation(self):
+    return
+
+  def chooseAction(self):
+    return
+
+class getDefensiveActions(Actions):
+  def __init__(self):
+    return
+  def defenceProbability(self):
+    return
+  def selectPatrolTarget(self):
+    return
   def chooseAction(self, gameState):
     """
-    Picks among actions randomly.
-    """
+        Picks among actions randomly.
+        """
     actions = gameState.getLegalActions(self.index)
 
     '''
@@ -90,3 +98,28 @@ class DummyAgent(CaptureAgent):
 
     return random.choice(actions)
 
+class Attacker():
+
+  def __init__(self):
+    return
+
+  def registerInitialState(self, gameState):
+    CaptureAgent.registerInitialState(self, gameState)
+    self.DefenceStatus = getDefensiveActions(self, self.index, gameState)
+    self.OffenceStatus = getOffensiveActions(self, self.index, gameState)
+
+  def chooseAction(self, gameState):
+    return
+
+class Defender():
+  def __init__(self, index):
+    CaptureAgent.__init__(self, index)
+
+
+  def registerInitialState(self, gameState):
+    CaptureAgent.registerInitialState(self, gameState)
+    self.DefenceStatus = getDefensiveActions(self, self.index, gameState)
+    self.OffenceStatus = getOffensiveActions(self, self.index, gameState)
+
+  def chooseAction(self, gameState):
+    return
